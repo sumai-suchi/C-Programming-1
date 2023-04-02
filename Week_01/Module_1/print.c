ff4b6f823ba665c32c02f31fd42ff1446a9bd754
DEWAN <dewanmahinchy@gmail.com>
2023-04-03 00:09:41 +0600

    checking code

--- Week_01/Module_1/print.c
+++ Week_01/Module_1/print.c
@@ -4,7 +4,7 @@ int main()
     // printf("Hello World\n");
     // printf("Now I am a programmer \\ %%");
     // printf("Hello world\nI am D1\nNew line a jao");
-    printf("Hello world\tI am D1");
+    printf("Hello world\tI am D1\nI'm Baba Yaga");
     return 0;
 }
 
