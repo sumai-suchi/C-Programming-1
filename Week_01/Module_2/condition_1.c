9e73efcce70bcafbf5985678e0e9c02150e86010
DEWAN <dewanmahinchy@gmail.com>
2023-04-03 03:40:15 +0600

    added condition_1.c file

---
+++ Week_01/Module_2/condition_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if(tk >= 100){
+        printf("I will eat burger");
+    }else{
+        printf("I will not eat whole day");
+    }
+
+    return 0;
+}
