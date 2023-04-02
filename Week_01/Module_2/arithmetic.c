fd0b983ce09b1efd52eeb75bf94fd5abb6aadfd7
DEWAN <dewanmahinchy@gmail.com>
2023-04-03 02:41:45 +0600

    added arithmetic.c file

---
+++ Week_01/Module_2/arithmetic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a=10, b=20;
+    /* int sum = a+b;
+    printf("%d", sum); */
+    /* int min = b-a;
+    printf("%d", min); */
+    /* int multi = a*b;
+    printf("%d", multi); */
+    /* int x=3, y=20;
+    float div = y*1.0/x;
+    printf("%f", div); */
+    int k=3, n=20;
+    int mod = n%k;
+    printf("%d", mod);
+
+    return 0;
+}
