264bd5e76886d8870efdc91c9125a96ce52b2f14
DEWAN <dewanmahinchy@gmail.com>
2023-04-02 23:50:34 +0600

    added print.c file

---
+++ Week_01/Module_1/print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // printf("Hello World\n");
+    // printf("Now I am a programmer \\ %%");
+    // printf("Hello world\nI am D1\nNew line a jao");
+    printf("Hello world\tI am D1");
+    return 0;
+}
+
+// \n - new line
+// \t - tab
+// % - %% (Have to give double percentage to show a percent symbol)
+/* \ - \\ (Have to give double backslash to show a backslash symbol)*/
