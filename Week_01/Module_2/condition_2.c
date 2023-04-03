4b638377b606e1e244aab8baebf69f18fe7c7c59
DEWAN <dewanmahinchy@gmail.com>
2023-04-03 17:49:13 +0600

    added condition_2 file

---
+++ Week_01/Module_2/condition_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int tk;
+    scanf("%d", &tk);
+    if(tk >= 100){
+        printf("I'll eat burger");
+    }else if(tk >= 50){
+        printf("I'll eat fuchka");
+    }else{
+        printf("I will eat nothing");
+    }
+    
+
+    return 0;
+}
