7955d5d529abfe22b04ccef325cf7fe377700280
DEWAN <dewanmahinchy@gmail.com>
2023-04-05 01:48:07 +0600

    added practicePrb_5.c file

---
+++ Week_01/Module_2.5/practicePrb_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int amount;
+    scanf("%d", &amount);
+    if(amount >= 10000){
+        printf("Gucci Bag\n");
+
+        if(amount > 20000){
+            printf("Gucci Belt");
+        }
+    }else if(amount >= 5000){
+        printf("Levis Bag");
+    }else{
+        printf("Something.");
+    }
+
+
+
+    return 0;
+}
