72243357f419465c4b96ac4c534884ed1e33e5dc
DEWAN <dewanmahinchy@gmail.com>
2023-04-10 22:02:44 +0600

    added char_PS.c file

---
+++ Week_02/Module_5/char_PS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    char givenChar;
+    scanf("%c", &givenChar);
+    if(givenChar >= 65 && givenChar <= 90){
+        char lowerChar = givenChar + 32;
+        printf("%c", lowerChar);
+    }else if(givenChar >= 97 && givenChar <= 122){
+        char upperChar = givenChar - 32;
+        printf("%c", upperChar);
+    }
+
+    return 0;
+}
