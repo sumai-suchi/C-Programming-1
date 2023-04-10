3ddf4585f144782784b62a7149cb62d0f2c57427
DEWAN <dewanmahinchy@gmail.com>
2023-04-11 01:14:43 +0600

    added minAndMax_PS.c file

---
+++ Week_02/Module_5/minAndMax_PS.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    // Minimum
+    if(a <= b && a <= c){
+        printf("%d ", a);
+    }else if(b <= a && b <= c){
+        printf("%d ", b);
+    }else{
+        printf("%d ", c);
+    }
+    // Maximum
+    if(a >= b && a >= c){
+        printf("%d", a);
+    }else if(b >= a && b >= c){
+        printf("%d", b);
+    }else{
+        printf("%d", c);
+    }
+
+    return 0;
+}
+
+// find minimum and maximum number from 3 integer
