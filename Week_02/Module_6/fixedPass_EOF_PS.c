56d169c2d4a400a137eb1e2678ed1d521f9071a4
DEWAN <dewanmahinchy@gmail.com>
2023-04-12 03:25:07 +0600

    added fixedPass_EOF_PS.c file

---
+++ Week_02/Module_6/fixedPass_EOF_PS.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    while(scanf("%d", &x) != EOF){
+        if(x == 1999){
+            printf("Correct\n");
+            break;
+        }else{
+            printf("Wrong\n");
+        }
+    }
+    
+
+    return 0;
+}
